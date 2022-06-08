69f6fea4764b1926d3552ab374cbd027009ea87e
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2022-06-08 20:37:38 +0530

    Updated vehicle detection module and added a graphical version for same

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -3,41 +3,34 @@ import cv2      # For Computer Vision
 import csv      # For writing data in file
 import collections  # For easy output processing
 import numpy as np  # For processing images easily
+import time
+# import sched
+# import schedule
 
-
-# declare favourable size of image so that it can be processed by the YOLO
 input_size = 320
 
+# Time calculation variables
+delayTime = 1
+totalVehicles = 0
 
-# Detection confidence threshold
+# thresholds
 confThreshold = 0.4
 nmsThreshold = 0.4
 
-
-# Store Coco Names in a list
 classes = "coco.names"
 classNames = open(classes).read().strip().split('\n')
 
-
-# class index for our required detection classes
 required_class_index = [1, 2, 3, 5, 7] # This is 0 index based (check coco names file)
 detected_classNames = []
-
 
 # model and weight files
 modelConfiguration = 'yolov3-320.cfg'
 modelWeigheights = 'yolov3-320.weights'
 
 
-# configure the dnn model for yolo3
-yolo = cv2.dnn.readNet(modelConfiguration, modelWeigheights)
+# configure yolo3
+yolo = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeigheights)
 
-
-# Define random colour for each class
-colors = np.random.randint(0, 255, size=(len(classNames), 3), dtype='uint8')
-
-
-# Function for finding the detected objects from the network output
 def processor(outputs,img):
     global detected_classNames
     detected_classNames = []
@@ -67,31 +60,26 @@ def processor(outputs,img):
             detected_classNames.append(name)
 
 
-image_files = ['./images/image_1.jpg', './images/image_2.jpg', './images/image_3.jpg', './images/image_4.jpg']
+image_files = ['./images/image_1.jpg', './images/image_2.jpg', './images/image_3.jpg', './images/image_4.jpg', './images/image_6.jpeg', './images/image_7.jpeg', './images/image_8.jpeg', './images/image_9.jpeg']
 
 
 def vehicle_detector(image):
     img = cv2.imread(image)
     blob = cv2.dnn.blobFromImage(img, 1 / 255, (input_size, input_size), [0, 0, 0], 1, crop=False)
 
-    # Set the input of the network
     yolo.setInput(blob)
     layersNames = yolo.getLayerNames()
     outputNames = [(layersNames[i - 1]) for i in yolo.getUnconnectedOutLayers()]
-    # Feed data to the network
-    outputs = yolo.forward(outputNames)
 
-    # Find the objects from the network output
+    outputs = yolo.forward(outputNames)
     processor(outputs,img)
 
-    # count the frequency of detected classes
     frequency = collections.Counter(detected_classNames)
-    print(frequency)
-    print("========================")
+    # print(frequency)
+    global totalVehicles
+    totalVehicles += frequency['car'] + frequency['motorbike'] + frequency['truck'] + frequency['bicycle'] + frequency['bus']
+    # print("========================")
     # print(detected_classNames)
-
-    # In case user wants to get output for each image only when he/she press a key 
-    # cv2.waitKey(0)
 
     # save the data to a csv file
     with open("data.csv", 'a') as f1:
@@ -99,13 +87,35 @@ def vehicle_detector(image):
         cwriter.writerow("================================================")
         cwriter.writerow([image," Information ->"])
         cwriter.writerow(["Cars -> " + str(frequency['car'])])
-        cwriter.writerow(["Bikes -> " + str(frequency['motorbike']) ])
+        cwriter.writerow(["Bikes -> " + str(frequency['motorbike'])])
         cwriter.writerow(["bicycle -> " + str(frequency['bicycle'])])
         cwriter.writerow(["Buses -> " + str(frequency['bus'])])
         cwriter.writerow(["Trucks -> " + str(frequency['truck'])])
-    f1.close()
+    f1.close()    
 
 
-if __name__ == '__main__':
+def timeAlloter():
     for i in range(len(image_files)):
         vehicle_detector(image_files[i])
+    global totalVehicles
+    print("Total number of vehicles =", totalVehicles)
+    print("========================")
+    global delayTime
+    delayTime = totalVehicles/10
+    totalVehicles = 0
+
+if __name__ == '__main__':
+    # for i in range(len(image_files)):
+    #     vehicle_detector(image_files[i])
+
+    # while(True):
+    #     starter = thd.Timer(delayTime, function_caller)
+    #     starter.start()
+
+    # schduler = sched.scheduler()
+    # schedule.every(delayTime).seconds.do(function_caller)
+
+    while(True):
+        # schedule.run_pending()
+        time.sleep(delayTime)
+        timeAlloter()
